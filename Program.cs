bd5b6c660b2ca2ad70c8230d3a4908688d4ee737
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-03-25 22:06:23 +0000

    OOP 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿// OOP
 var p1 = new Person("Joao", "Maia", new DateOnly(1980, 1, 1));
-var p2 = new Person("Maria", "Maia", new DateOnly(1990, 1, 1));
+var p2 = new Person("Maria", "Bunita", new DateOnly(1990, 1, 1));
 
 List<Person> people = [p1, p2];
 Console.WriteLine(people.Count);
+
+// Adding pets to people
+p1.Pets.Add(new Dog("Scooby"));
+p2.Pets.Add(new Cat("Oliver"));
+p2.Pets.Add(new Cat("Doraemon"));
+
+foreach(var person in people)
+{
+    Console.WriteLine(person); 
+    foreach(var pet in person.Pets)
+    {
+        Console.WriteLine($"Has {pet}");
+    }
+}
 
 // Creating a class
 class Person(string firstName, string lastName, DateOnly birthday)
@@ -11,4 +25,40 @@ class Person(string firstName, string lastName, DateOnly birthday)
     public string FirstName { get; } = firstName;
     public string LastName { get; } = lastName;
     public DateOnly Birthday { get; } = birthday;
+    public List<Pet> Pets { get; } = new();
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
+
+public abstract class Pet(string name)
+{
+    public string PetName { get; } = name;
+
+    public abstract string MakeNoise();
+    public override string ToString()
+    {
+        return $"{PetName}, a {GetType().Name.ToLower()}";
+    }
+}
+
+public class Cat(string name) : Pet(name)
+{
+
+    public string Name { get; } = name;
+    public override string MakeNoise()
+    {
+        return "Meow";
+    }
+}
+
+public class Dog(string name) : Pet(name)
+{
+    public string Name { get; } = name;
+
+    public override string MakeNoise()
+    {
+        return "Bark";
+    }
 }
--- obj/Debug/net8.0/c-sharp-learn.AssemblyInfo.cs
+++ obj/Debug/net8.0/c-sharp-learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp-learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c42a3c137547ff2abc7c657fe019014a9ff9fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2552803d3e7e03472a44dde80a4ff5e25a9c69ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp-learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp-learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
