72751506f6f4a95f3ba695ed09bb6586234b5138
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-03-25 13:02:05 +0000

    Strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Strings
+string firstPerson = "Maria";
+string secondPerson = "John";
+Console.WriteLine($"Their names are {firstPerson} and {secondPerson}");
+
+// String.Trim
+string spacedName = "       Hi there       ";
+Console.WriteLine($"No trim: {spacedName}");
+Console.WriteLine($"TrimStart: {spacedName.TrimStart()}");
+Console.WriteLine($"TrimEnd: {spacedName.TrimEnd()}");
+Console.WriteLine($"Trim: {spacedName.Trim()}");
+
+// String.Replace
+string names = $"Their names are {firstPerson} and {secondPerson}";
+Console.WriteLine(names.Replace("John", "Marco"));
+Console.WriteLine(names);
+
+// String.Contain
+Console.WriteLine(names.Contains("Maria"));
+Console.WriteLine(names.Contains("Julian"));
+
+// Formatting and length
+Console.WriteLine(names.ToUpper());
+Console.WriteLine(names.ToLower());
+Console.WriteLine(names.Length);
--- README.md
+++ README.md
@@ -10,3 +10,11 @@ In terminal, run:
 ```bash
 dotnet new console
 ```
+This will create the necessary files for a basic console project. Then, edit the code in ```Program.cs``` and
+```bash
+dotnet run
+```
+to compile and see the results in terminal.
+
+## Strings
+Strings are immutable (unless assigned). Using methods such as ```String.Replace``` will return a new string and the original will be intact. Some string methods explored: ```Trim``` and it's variants, ```Replace``` and ```Contain```.
